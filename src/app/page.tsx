6dbfd708a6695bb60a002eb58a8ea04442de454e
EzraPaz <johnezrapaz@gmail.com>
2025-04-03 13:41:23 +0800

    init project for firstmate coding exam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,145 @@
-import Image from "next/image";
-
+"use client";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import _ from "lodash";
+import { ChangeEvent, useState } from "react"
+interface Data {
+  delay: number | "";
+  delayType: string;
+  message: string;
+  webhook: string;
+}
 export default function Home() {
+  const [data, setData] = useState<Data>({
+    delay: "",
+    delayType: "",
+    message: "",
+    webhook: "",
+  });
+
+  const handleDelayTypeChange = (value: string) =>{
+    setData({
+      ...data,
+      delayType: value,
+    })
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value,
+    })
+  };
+
+  const handleSend = () => {
+    const { delay, delayType, message, webhook } = data;
+    if (!delay || !message || !webhook) return;
+
+    const delayTime = calculateDelay(Number(delay) || 0, delayType);
+
+    setTimeout(async () => {
+      try {
+        console.log('rasdfasdf')
+        console.log('adfasdfas')
+        const response = await fetch(webhook, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: `From John Ezra Paz's Slack Bot: ${message}`  }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to send message");
+        }
+
+        alert("Message sent successfully!");
+      } catch (error) {
+        alert("Error sending message");
+        console.error("Error:", error);
+      } finally {
+        handleClear()
+      }
+    }, delayTime);
+  }
+
+  const handleClear = () => {
+    setData({
+      delay: "",
+      delayType: "",
+      message: "",
+      webhook: "",
+    })
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Send Delayed Messages</CardTitle>
+        <CardDescription>This project enables you to send delayed messages using slack api</CardDescription>
+      </CardHeader>
+      <CardContent>
+          <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="framework">Delay</Label>
+            <div className="flex flex-row space-x-2">
+              <Input id="delay" value={data.delay} type="number" placeholder="Add delay" onChange={(e)=>handleChange(e)} />
+              <Select value={data.delayType} onValueChange={handleDelayTypeChange}>
+                <SelectTrigger id="delayType">
+                  <SelectValue placeholder="Delay type" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="seconds">seconds</SelectItem>
+                  <SelectItem value="minutes">minutes</SelectItem>
+                  <SelectItem value="hours">hours</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="message">Message</Label>
+              <Input id="message" placeholder="Enter Message" value={data.message} onChange={(e)=>handleChange(e)} />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="webhook">Webhook URL</Label>
+              <Input id="webhook" placeholder="Enter Webhook URL" value={data.webhook} onChange={(e)=>handleChange(e)} />
+            </div>
+          </div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button onClick={()=>handleSend()}>{data.delay && data.delayType ? `Send in ${data.delay} ${data.delayType}` : 'Send'}</Button>
+        <Button onClick={handleClear} variant="outline">Clear</Button>
+      </CardFooter>
+    </Card>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
+const calculateDelay = (delay: number, unit: string) => {
+  const timeMultipliers = {
+    seconds: 1000,
+    minutes: 60 * 1000,
+    hours: 60 * 60 * 1000,
+  };
+
+  return delay * _.get(timeMultipliers, unit, 1000); // Default to seconds
+};
